8c1b595521d58ddb30f1df1457e1cc9d2f74207d
shell <missing_shell@126.com>
2024-04-19 21:19:16 +0800

    Initialize the repository

---
+++ main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_log.h"
+
+static const char TAG[] = "MAIN";
+
+void app_main()
+{
+}
